ae7fbbc8ac85a8fe16bba4a6494c88f77188714a
jaabless <jefferyasamoahbless@gmail.com>
2025-04-24 18:33:00 +0000

    Add EmployeeNotFoundException to handle missing employee cases

---
+++ src/exceptions/EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+// EmployeeNotFoundException.java
+public class EmployeeNotFoundException extends Exception {
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+}
