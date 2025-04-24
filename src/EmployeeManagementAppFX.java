ed823fbf0a993faab952e62269fa95f3e2dc2ba4
jaabless <jefferyasamoahbless@gmail.com>
2025-04-24 21:17:16 +0000

    Connect JavaFX to the application

--- src/EmployeeManagementAppFX.java
+++ src/EmployeeManagementAppFX.java
@@ -5,11 +5,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import ui.DialogFactory;
+import ui.TableUtils;
 
 public class EmployeeManagementAppFX extends Application {
-    private EmployeeDatabase database = new EmployeeDatabase();
-    private TableView<Employee<Integer>> table = new TableView<>();
-
     public static void main(String[] args) {
         launch(args);
     }
@@ -30,33 +29,6 @@ public class EmployeeManagementAppFX extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
-
-//        Button advancedFilterBtn = UIFactory.createStyledButton("Advanced Filter");
-//        advancedFilterBtn.setOnAction(e ->DialogFactory.showAdvancedFilterDialog() );
-//
-//        Button resetBtn = UIFactory.createStyledButton("Reset Table");
-//        resetBtn.setOnAction(e -> DialogFactory.showResetDialog());
-//
-//        Button averageBtn = UIFactory.createStyledButton("Calculate Average");
-//        averageBtn.setOnAction(e -> DialogFactory.showAverageDialog());
-//
-//        Button raiseBtn = UIFactory.createStyledButton("Raise");
-//        raiseBtn.setOnAction(e -> DialogFactory.showGiveRaiseDialog());
-//
-//        Button getTop5 = UIFactory.createStyledButton("Highest Earners");
-//        getTop5.setOnAction(e -> DialogFactory.showTopPaidDialog());
-//
-//        Button sortPerformanceBtn = UIFactory.createStyledButton("Sort by Performance");
-//        sortPerformanceBtn.setOnAction(e -> DialogFactory.showSortByPerfomance());
-//
-////
-//        HBox btnBox = UIFactory.createHBox(advancedFilterBtn, resetBtn,averageBtn,getTop5,raiseBtn,sortPerformanceBtn);
-
-//        VBox root1 = UIFactory.createVBox(table, btnBox);
-//        root1.setPadding(new Insets(20, 0, 0, 0));
-//
-//        primaryStage.setScene(new Scene(root1, 800, 600));
-//        primaryStage.show();
     }
 
 
