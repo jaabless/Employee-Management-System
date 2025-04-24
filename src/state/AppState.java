b1c92230d21748a846e485c5a1074bceafd63a61
jaabless <jefferyasamoahbless@gmail.com>
2025-04-24 20:44:29 +0000

    Add state management

--- src/state/AppState.java
+++ src/state/AppState.java
@@ -1,12 +1,15 @@
+package state;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableView;
+import models.Employee;
+import services.EmployeeDatabase;
 
 public class AppState {
     public static final EmployeeDatabase<Integer> database = new EmployeeDatabase<>();
     public static TableView<Employee<Integer>> tableView;
 
-    // Final because we never reassign it — only update its contents
     public static  ObservableList<Employee<Integer>> observableList =
             FXCollections.observableArrayList(database.getAllEmployees());
 
